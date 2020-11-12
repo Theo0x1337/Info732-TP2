5df6e22fea74c2e76153a71cf282c8762cc11f50
KevinIDU <56953169+KevinIDU@users.noreply.github.com>
2020-11-12 15:18:02 +0100

    Add files via upload

---
+++ casque.java
@@ -0,0 +1,19 @@
+
+public class casque {
+	
+	int id;
+	String grade;
+	String etat;
+	String forme;
+	
+	
+	public casque(int identifiant, String grade, String etat, String forme ) {
+		this.id = identifiant;
+		this.grade = grade;
+		this.etat = etat;
+		this.forme = forme;
+		
+		
+	}
+	
+}
---
+++ musee.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class musee {
+
+	ArrayList<String> listeCasque = new ArrayList<String>();
+	
+	
+	
+
+}
